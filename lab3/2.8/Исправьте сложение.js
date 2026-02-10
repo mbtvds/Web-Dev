ffa16b29eb665f034e2cfccc39fcb4a4d271cbfd
mbtvds <umbetov.dias@mail.ru>
2026-02-10 23:36:35 +0500

    Create Исправьте сложение.js

---
+++ lab3/2.8/Исправьте сложение.js
@@ -0,0 +1,14 @@
+let a = "1"; // prompt("Первое число?", 1);
+let b = "2"; // prompt("Второе число?", 2);
+
+alert(a + b); // 12
+
+let a = +prompt("Первое число?", 1);
+let b = +prompt("Второе число?", 2);
+
+alert(a + b); // 3
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert(+a + +b); // 3
